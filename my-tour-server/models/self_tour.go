be423a8f37a9043a36c80f7323d04dbba455e304
周翔辉 <3236432-fankaljead@users.noreply.gitlab.com>
2020-04-12 14:59:18 +0800

    change model add some json format

--- my-tour-server/models/self_tour.go
+++ my-tour-server/models/self_tour.go
@@ -17,18 +17,17 @@ type SelfTour struct {
 }
 
 type SelfTourDays struct {
-	Id            int64
-	Title         string
-	Descirption   string
-	DayNumber     int
-	SelfTourId    int64
-	ScenarySpotId int64
-	CreateUserId  int64
+	Id            int64     `json:"id"`
+	Title         string    `json:"title"`
+	Descirption   string    `json:"descirption"`
+	DayNumber     int       `json:"day_number"`
+	SelfTourId    int64     `json:"self_tour_id"`
+	ScenarySpotId int64     `json:"scenary_spot_id"`
+	CreateUserId  int64     `json:"create_user_id"`
 	CreateUptime  time.Time `json:"create_time" orm:"auto_now_add;type(datetime)"`
 }
 
 func init() {
 	orm.RegisterModel(new(SelfTour), new(SelfTourDays))
-	AddATable("self_tour")
-	AddATable("self_tour_days")
+	AddATable("self_tour", "self_tour_days")
 }

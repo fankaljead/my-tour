a1e55be8a459806c3f9cdc7ce972eea54b139534
周翔辉 <3236432-fankaljead@users.noreply.gitlab.com>
2020-05-02 22:44:11 +0800

    add travel note brief

--- my-tour/src/components/homepage/TravelNoteBrief.vue
+++ my-tour/src/components/homepage/TravelNoteBrief.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <div class="travel_note_left">
-      <img alt="" src="data.img_src" />
+      <!-- <img alt="" v-bind:src="data.img" /> -->
+      <van-image
+        width="15rem"
+        height="15rem"
+        fit="contain"
+        v-bind:src="data.img"
+      >
+      </van-image>
     </div>
     <div class="travel_note_right">
       <div>
-        <h1>{{ data.title }}</h1>
+        <h2>{{ data.title }}</h2>
         <p>
           {{ data.intro }}
         </p>
@@ -21,12 +28,24 @@
 </template>
 
 <script>
+import Vue from 'vue';
+import { Image as VanImage } from 'vant';
+import { Card } from 'vant';
+
+Vue.use(Card);
+
+Vue.use(VanImage);
+
 export default {
   name: "TravelNoteBrief",
-  data(props) {
-    return {
-      data: props.data
-    };
+  props: {
+    data: {
+      author: String,
+      title: String,
+      intro: String,
+      img: String,
+      time: String
+    }
   }
 };
 </script>
@@ -34,9 +53,13 @@ export default {
 <style>
 .travel_note_left {
   width: 20%;
-  background-color: #12f32f;
+  /* background-color: #12f32f; */
   height: 100px;
   float: left;
+}
+.travel_note_left img {
+  width: 100px;
+  height: 100px;
 }
 .travel_note_right {
   width: 80%;
--- my-tour/src/views/HomePage.vue
+++ my-tour/src/views/HomePage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     This Home Page
-    <TravelNoteBrief />
+    <TravelNoteBrief v-bind:data="data" />
   </div>
 </template>
 
@@ -13,6 +13,17 @@ export default {
   name: 'HomePage',
   components: {
     TravelNoteBrief
+  },
+  data() {
+    return {
+      data: {
+        author: "游乐王子",
+        title: "摸仙煲一日游",
+        intro: "摸仙煲一日游摸仙煲一日游摸仙煲一日游摸仙煲一日游摸仙煲一日游",
+        img: 'https://imgsa.baidu.com/forum/w%3D580/sign=b99e7da20355b3199cf9827d73a88286/a6895a540923dd54401396cbd809b3de9d82484e.jpg',
+        time: "2020-05-02",
+      }
+    }
   }
 }
 </script>
